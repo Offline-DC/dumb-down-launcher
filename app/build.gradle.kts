027b152885183983b477f8252f0cf3f8081c2a3d
Jack Nugent <31449177+jacknugent@users.noreply.github.com>
2026-03-06 10:58:15 -0500

    Bump to version 2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.offlineinc.dumbdownlauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "2.3"
+        versionCode = 8
+        versionName = "2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
