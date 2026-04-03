1e9898f157316b4d01aff733f826f4729e399877
Jack Nugent <31449177+jacknugent@users.noreply.github.com>
2026-04-03 09:41:31 -0400

    Add quack tweak

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.offlineinc.dumbdownlauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 39
-        versionName = "3.23.0"
+        versionCode = 40
+        versionName = "3.24.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
