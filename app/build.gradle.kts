fa08b4e7a66bd10f98173429d52e58b36849c600
Jack Nugent <jack@offline.community>
2026-05-18 10:53:21 -0400

    Add beta version for testing mouse tutorial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.offlineinc.dumbdownlauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 124
-        versionName = "4.64.0"
+        versionCode = 125
+        versionName = "4.65.0-beta.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
