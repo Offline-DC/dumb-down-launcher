93520de37c00b1d89a2923767fc004266c5171e6
Jack Nugent <jack@offline.community>
2026-05-20 09:42:59 -0400

    Add free up space test version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.offlineinc.dumbdownlauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 130
-        versionName = "4.68.0-beta.0"
+        versionCode = 131
+        versionName = "4.68.0-beta.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
