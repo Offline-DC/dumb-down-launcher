98a70abea4f7f6664f86c1f9bd2c77fd496d3eaf
Jack Nugent <jack@offline.community>
2026-05-26 09:33:44 -0400

    add tct disable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.offlineinc.dumbdownlauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 139
-        versionName = "v4.71.0"
+        versionCode = 141
+        versionName = "v4.72.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
