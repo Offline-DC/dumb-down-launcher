0b15b553e323c43962c673d6511c2b8a58aaaba7
Jack Nugent <jack@offline.community>
2026-06-03 11:40:11 -0400

    Bump beta version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.offlineinc.dumbdownlauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 153
-        versionName = "v4.80.0-beta.3"
+        versionCode = 154
+        versionName = "v4.80.0-beta.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
