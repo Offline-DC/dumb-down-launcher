265b95854107554ecf0fa66808b527eea7109125
Jack Nugent <jack@offline.community>
2026-06-03 13:45:38 -0400

    Make update sticky

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.offlineinc.dumbdownlauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 154
-        versionName = "v4.80.0-beta.4"
+        versionCode = 155
+        versionName = "v4.80.0-beta.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
